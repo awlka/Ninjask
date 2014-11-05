f1f8c06d88f19399ed4a67c58d06948d0326b2c4
Lucas <lucas@agtlucas.com>
2014-11-04 23:22:00 -0200

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/**
+ * Duskull Gruntfile!
+ */
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Show elapsed time after tasks run
+  require('time-grunt')(grunt);
+  // Load all Grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Uglify!
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'assets/js/**/*.js',
+        dest: 'assets/build/js/a.js'
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
+          style: 'compressed'
+        },
+        files: {
+          'assets/build/css/a.css': 'assets/_scss/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      uglify: {
+        files: ['assets/js/**/*.js'],
+        tasks: ['uglify']
+      },
+      sass: {
+        files: ['assets/_scss/**/*.{scss,sass}'],
+        tasks: ['sass']
+      }
+    }
+
+  });
+
+  grunt.registerTask('js', ['uglify']);
+  grunt.registerTask('style', ['sass']);
+
+  grunt.registerTask('default', ['watch']);
+
+};
